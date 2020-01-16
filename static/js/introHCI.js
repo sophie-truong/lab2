9340faab00e4bda4b6636512c5efb532a701381d
Sophie Truong <sbt001@ucsd.edu>
2020-01-16 09:19:37 -0800

    added Javascript!

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -2,8 +2,21 @@ $(document).ready(function() {
     initializePage();
 });
 
+
 function initializePage() {
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
+            $("a.thumbnail").click(projectClick);
             });
+    		// Add any additional listeners here
+    		// example: $("#div-id").click(functionToCall);
 }
+
+function projectClick(e) {
+	// prevent page from reloading
+	e.preventDefault();
+	// In an event handler, $(this) refers to 
+	// the object that triggered the event
+	$(this).css("background-color", '#7fff00');
+}
+
